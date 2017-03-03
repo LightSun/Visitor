920187e870b724dd5b9575b2501639b237de1e0e
LinghtSun <532278976@qq.com>
2017-03-03 16:43:18 +0800

    change doc

--- Visitor/src/main/java/com/heaven7/java/visitor/util/Collections2.java
+++ Visitor/src/main/java/com/heaven7/java/visitor/util/Collections2.java
@@ -3,7 +3,11 @@ package com.heaven7.java.visitor.util;
 import java.util.Collection;
 import java.util.List;
 import java.util.SortedSet;
-
+/**
+ * expand util class of Collections.
+ * @author heaven7
+ * @since 1.1,.2
+ */
 public class Collections2 {
 
 	public static <T> List<T> asList(Collection<T> coll){
