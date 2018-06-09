4d8443182d98d58d6fa4f033ab77244c29779cf1
lightsun <978136772@qq.com>
2018-06-09 11:19:30 +0800

    add some PileVisitors of mathematical operation. in PileVisitor

--- Visitor/src/main/java/com/heaven7/java/visitor/PileVisitor.java
+++ Visitor/src/main/java/com/heaven7/java/visitor/PileVisitor.java
@@ -6,4 +6,126 @@ package com.heaven7.java.visitor;
  * @since 1.2.0
  */
 public interface PileVisitor<T> extends Visitor2<Object, T, T, T> {
+
+    /**
+     * the pile visitor of 'int-add '
+     * @since 1.2.5
+     */
+    PileVisitor<Integer> INT_ADD = new PileVisitor<Integer>() {
+        @Override
+        public Integer visit(Object o, Integer integer, Integer integer2) {
+            return integer + integer2;
+        }
+    };
+    /**
+     * the pile visitor of 'int-sub'
+     * @since 1.2.5
+     */
+    PileVisitor<Integer> INT_SUB = new PileVisitor<Integer>() {
+        @Override
+        public Integer visit(Object o, Integer integer, Integer integer2) {
+            return integer - integer2;
+        }
+    };
+    /**
+     * the pile visitor of 'int-multi'
+     * @since 1.2.5
+     */
+    PileVisitor<Integer> INT_MULTI = new PileVisitor<Integer>() {
+        @Override
+        public Integer visit(Object o, Integer integer, Integer integer2) {
+            return integer * integer2;
+        }
+    };
+    /**
+     * the pile visitor of 'int-div'
+     * @since 1.2.5
+     */
+    PileVisitor<Integer> INT_DIV = new PileVisitor<Integer>() {
+        @Override
+        public Integer visit(Object o, Integer integer, Integer integer2) {
+            return integer / integer2;
+        }
+    };
+    /**
+     * the pile visitor of 'float-add'
+     * @since 1.2.5
+     */
+    PileVisitor<Float> FLOAT_ADD = new PileVisitor<Float>() {
+        @Override
+        public Float visit(Object o, Float f1, Float f2) {
+            return f1 + f2;
+        }
+    };
+    /**
+     * the pile visitor of 'float-sub'
+     * @since 1.2.5
+     */
+    PileVisitor<Float> FLOAT_SUB = new PileVisitor<Float>() {
+        @Override
+        public Float visit(Object o, Float f1, Float f2) {
+            return f1 - f2;
+        }
+    };
+    /**
+     * the pile visitor of 'float-multi'
+     * @since 1.2.5
+     */
+    PileVisitor<Float> FLOAT_MULTI = new PileVisitor<Float>() {
+        @Override
+        public Float visit(Object o, Float f1, Float f2) {
+            return f1 * f2;
+        }
+    };
+
+    /**
+     * the pile visitor of 'float-div'
+     * @since 1.2.5
+     */
+    PileVisitor<Float> FLOAT_DIV = new PileVisitor<Float>() {
+        @Override
+        public Float visit(Object o, Float f1, Float f2) {
+            return f1 / f2;
+        }
+    };
+    /**
+     * the pile visitor of 'double-add'
+     * @since 1.2.5
+     */
+    PileVisitor<Double> DOUBLE_ADD = new PileVisitor<Double>() {
+        @Override
+        public Double visit(Object o, Double f1, Double f2) {
+            return f1 + f2;
+        }
+    };
+    /**
+     * the pile visitor of 'double-sub'
+     * @since 1.2.5
+     */
+    PileVisitor<Double> DOUBLE_SUB = new PileVisitor<Double>() {
+        @Override
+        public Double visit(Object o, Double f1, Double f2) {
+            return f1 - f2;
+        }
+    };
+    /**
+     * the pile visitor of 'double-multi'
+     * @since 1.2.5
+     */
+    PileVisitor<Double> DOUBLE_MULTI = new PileVisitor<Double>() {
+        @Override
+        public Double visit(Object o, Double f1, Double f2) {
+            return f1 * f2;
+        }
+    };
+    /**
+     * the pile visitor of 'double-div'
+     * @since 1.2.5
+     */
+    PileVisitor<Double> DOUBLE_DIV = new PileVisitor<Double>() {
+        @Override
+        public Double visit(Object o, Double f1, Double f2) {
+            return f1 / f2;
+        }
+    };
 }
